fdde2a10ed7b086ec9e3625143ed5657bf18853f
chansikjeong <cjyong1004@naver.com>
2025-02-21 17:56:11 +0900

    update 의존성 끊기

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -7,7 +7,7 @@ import { RoomModule } from 'src/room/room.module';
 import { NoticeModule } from 'src/notice/notice.module'; // NoticeModule 임포트
 
 @Module({
-  imports: [forwardRef(() => RoomModule), NoticeModule],
+  imports: [forwardRef(() => RoomModule), forwardRef(() => NoticeModule)], // forwardRef로 감싸기
   providers: [GameService, RedisProvider, RoomService], // NightResultService 제거
   exports: [GameService],
 })
--- src/notice/notice.module.ts
+++ src/notice/notice.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { NightResultService } from './night-result.service';
 import { RoomModule } from 'src/room/room.module';
+import { GameModule } from 'src/game/game.module';
 
 @Module({
-  imports: [RoomModule],
+  imports: [forwardRef(() => GameModule), forwardRef(() => RoomModule)],
   providers: [NightResultService],
   exports: [NightResultService], // 다른 모듈에서 사용해야 한다면 exports에도 등록
 })
