d8ab95d3d8696d64d40258d15420923e53e2d984
chansikjeong <cjyong1004@naver.com>
2025-03-07 14:14:04 +0900

    Update redis.provider.ts

--- src/redis/redis.provider.ts
+++ src/redis/redis.provider.ts
@@ -5,8 +5,8 @@ export const RedisProvider = {
   provide: 'REDIS_CLIENT',
   useFactory: (): Redis => {
     return new Redis({
-      host: process.env.REDIS_HOST || 'localhost',
-      // host: '43.200.181.46',
+      // host: process.env.REDIS_HOST || 'localhost',
+      host: '43.200.181.46',
       port: Number(process.env.REDIS_PORT) || 6379,
     });
   },
