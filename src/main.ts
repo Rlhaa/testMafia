34e9cc0ed9dbc38034f1874fd7d8c8651db3499b
thiswizard <dkaand3318@gmail.com>
2025-03-10 19:56:59 +0900

    add sentry dsn 환경변수 설정

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ import { AllExceptionFilter } from './exception.filter';
 async function bootstrap() {
   // sentry 초기화
   Sentry.init({
-    dsn: "https://7093f1075ffe2a3854e91d0c6818ddc7@o4508924635185152.ingest.us.sentry.io/4508924637151232" ,
+    dsn: process.env.dsn ,
     tracesSampleRate: 1.0,
   });
 
-  throw new Error("해당 에러는 pineapple를 위해 일부로 발생시킴")
+  throw new Error("해당 에러는 마피아를 위해 일부로 발생시킴")
 
   const app = await NestFactory.create(AppModule);
   // 프로젝트에서 발생한 모든에러를 Sentry 필터 적용
